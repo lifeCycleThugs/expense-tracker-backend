d39eb01ed05c2a99906bf07043d3b9197ab7c547
suyash159 <s.maheshwari@braininventory.com>
2022-04-24 16:21:57 +0530

    apis

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hi");
+import bodyParser, { json } from 'body-parser'
+import express from 'express'
+const userRouter = require('./modules/user/user.routes')
+
+const app = express()
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use("/user", userRouter)
+
+app.listen(3000, () => {
+    console.log("server is running");
+    
+})
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,5 +1,37 @@
+import { Request } from "express";
+import { addUser, findByEmail } from "./user.dal";
+import { Login, Signup } from "./user.interfaces";
+import bcrypt from 'bcrypt'
+
 class UserController {
     constructor() { }
+    loginUser = async (req: Request) => {
+         try {
+             const data:Login={email:req.body.email,password:req.body.password}
+             const result =await findByEmail(data)
+             return result
+         } catch (error) {
+             
+         }
+    }
+    signUpUser = async (req: Request) => {
+        req.body.passwordHash = bcrypt.hashSync(req.body.password, 5)
+        console.log(req.body.passwordHash);
+        
+        try {
+            const data: Signup = {
+                name: req.body.name,
+                email: req.body.email,
+                passwordHash: req.body.passwordHash,
+                phoneNumber: req.body.phoneNumber
+            }
+            const result = await addUser(data)
+            return result
+        } catch (error) {
+            
+        }
+    } 
+    
 }
 
 export const Controller = new UserController();
